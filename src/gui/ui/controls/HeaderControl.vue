69947b562053da58a2e06b2235381f06643fa717
dark0n3 <dark.sonik1998@gmail.com>
2020-07-03 13:52:28 +0300

    Fix header field

--- src/gui/ui/controls/HeaderControl.vue
+++ src/gui/ui/controls/HeaderControl.vue
@@ -2,11 +2,11 @@
     <div>
         <div class="row" v-if="labelPosition === 'left'">
             <div class="col-md-8">
-                <h4 class="modal-title" :value="demo_value">{{control.label}}</h4>
+                <h4 class="modal-title" :value="demo_value">{{control.header}}</h4>
             </div>
         </div>
         <div v-else class="form-group">
-                <h4 class="modal-title" :value="demo_value">{{control.label}}</h4>
+                <h4 class="modal-title" :value="demo_value">{{control.header}}</h4>
         </div>
     </div>
 </template>
--- src/template/ui/common/controls/HeaderControl.vue
+++ src/template/ui/common/controls/HeaderControl.vue
@@ -2,13 +2,13 @@
     <div class="controlItemWrapper" :class="control.className" :data-control-name="control.name">
         <div class="controlItem row" :id="control.name" v-if="labelPosition === 'left'">
             <div class="col-md-8 input-group">
-                <h4 class="modal-title" :value="demo_value">{{control.label}}</h4>
+                <h4 class="modal-title" :value="demo_value">{{control.header}}</h4>
             </div>
         </div>
         <div class="controlItem row" :id="control.name" v-else>
             <div class="form-group col-md-12">
                 <div class="input-group">
-                    <h4 class="modal-title" :value="demo_value">{{control.label}}</h4>
+                    <h4 class="modal-title" :value="demo_value">{{control.header}}</h4>
                 </div>
             </div>
         </div>
